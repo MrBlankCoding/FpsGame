90734f8e724e1718c05985461564085319eb1c83
ethanzhall <hallethan156@gmail.com>
2023-06-20 15:54:36 -0400

    Added a player
    Added a player
    
    Added movement and camera controls
    
    Added building prefabs

---
+++ FpsGame/Assets/Scrips/FirstPersonController.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+   
+
+public class FirstPersonController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public Camera playerCamera;
+
+    public float fov = 60f;
+    public bool cameraCanMove = true;
+    public float mouseSensitivity = 2f;
+    float maxLookAngle = 50f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+    public bool playerCanMove = true;
+    public float walkSpeed = 5f;
+    float maxVelocityChange = 10f;
+
+
+    public bool enableSprint = true;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public float sprintSpeed = 15f;
+    float sprintDuration = 5f;
+    float sprintCooldown = .5f;
+    float sprintFOV = 80f;
+    float sprintFOVStepTime = 10f;
+
+    bool isSprinting = false;
+    float sprintRemaining;
+    bool isSprintCooldown = false;
+    float sprintCooldownReset;
+ 
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCamera.fieldOfView = fov;
+
+    }
+
+
+    float camRotation;
+
+    private void Update()
+    {
+
+        if(cameraCanMove)
+        {
+            yaw = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
+
+
+            pitch -= mouseSensitivity * Input.GetAxis("Mouse Y");
+
+            pitch = Mathf.Clamp(pitch, -maxLookAngle, maxLookAngle);
+
+            transform.localEulerAngles = new Vector3(0, yaw, 0);
+            playerCamera.transform.localEulerAngles = new Vector3(pitch, 0, 0);
+        }
+
+
+        if(enableSprint)
+        {
+            if(isSprinting)
+            {
+                playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, sprintFOV, sprintFOVStepTime * Time.deltaTime);
+            }
+            else
+            {
+                sprintRemaining = Mathf.Clamp(sprintRemaining += 1 * Time.deltaTime, 0, sprintDuration);
+            }
+
+            if(isSprintCooldown)
+            {
+                sprintCooldown -= 1 * Time.deltaTime;
+                if (sprintCooldown <= 0)
+                {
+                    isSprintCooldown = false;
+                }
+            }
+            else
+            {
+                sprintCooldown = sprintCooldownReset;
+            }
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (playerCanMove)
+        {
+
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+
+            if (enableSprint && Input.GetKey(sprintKey) && sprintRemaining > 0f && !isSprintCooldown)
+            {
+                targetVelocity = transform.TransformDirection(targetVelocity) * sprintSpeed;
+
+                Vector3 velocity = rb.velocity;
+                Vector3 velocityChange = (targetVelocity - velocity);
+                velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+                velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+                velocityChange.y = 0;
+
+                if (velocityChange.x != 0 || velocityChange.z != 0)
+                {
+                    isSprinting = true;
+
+                }
+
+                rb.AddForce(velocityChange, ForceMode.VelocityChange);
+            }
+            else
+            {
+                isSprinting = false;
+
+
+                targetVelocity = transform.TransformDirection(targetVelocity) * walkSpeed;
+
+                Vector3 velocity = rb.velocity;
+                Vector3 velocityChange = (targetVelocity - velocity);
+                velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+                velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+                velocityChange.y = 0;
+
+                rb.AddForce(velocityChange, ForceMode.VelocityChange);
+            }
+        }
+
+    
+    }
+
+}
