4a46746d2c3f5d87c60ce13600279fbe33a87af0
AndrewBrodsky2010 <136294908+AndrewBrodsky2010@users.noreply.github.com>
2023-06-22 12:26:55 -0400

    Added Heallth And Damage to Normal Zombie *DOES NOT WORK*

---
+++ FpsGame/Assets/Scrips/HealthAndDamage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Animations;
+
+public class HealthAndDamage : MonoBehaviour
+{
+    [SerializeField] private Animator Death;
+    [SerializeField] private string Dying = "Z_FallingBack";
+    public int health = 100;
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (health == 0)
+        {
+            Death.Play(Dying);
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Bullet")
+        {
+            health -= 50;        
+        }
+    }
+}
