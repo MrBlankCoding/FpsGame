dea441abf5d01d20c44c263ab624bc7755223790
ethanzhall <hallethan156@gmail.com>
2023-06-23 10:11:07 -0400

    FIxed the gun

--- FpsGame/Assets/Guns/Modern Guns - Handgun/_Demo Assets/SimpleShoot.cs
+++ FpsGame/Assets/Guns/Modern Guns - Handgun/_Demo Assets/SimpleShoot.cs
@@ -1,4 +1,4 @@
-�using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 [AddComponentMenu("Nokobot/Modern Guns/Simple Shoot")]
@@ -9,37 +9,41 @@ public class SimpleShoot : MonoBehaviour
     public float reloadTime = 1f;
     private bool isReloading = false;
 
-    [Header("Prefab Refrences")]
+    [Header("Prefab References")]
     public GameObject bulletPrefab;
     public GameObject casingPrefab;
     public GameObject muzzleFlashPrefab;
-    
-    [Header("Location Refrences")]
+
+    [Header("Location References")]
     [SerializeField] private Animator gunAnimator;
     [SerializeField] private Transform barrelLocation;
     [SerializeField] private Transform casingExitLocation;
 
     [Header("Settings")]
-    [Tooltip("Specify time to destory the casing object")] [SerializeField] private float destroyTimer = 2f;
-    [Tooltip("Bullet Speed")] [SerializeField] private float shotPower = 1000f;
-    [Tooltip("Casing Ejection Speed")] [SerializeField] private float ejectPower = 150f;
-    
+    [Tooltip("Specify time to destroy the casing object")]
+    [SerializeField] private float destroyTimer = 2f;
+    [Tooltip("Bullet Speed")]
+    [SerializeField] private float shotPower = 1000f;
+    [Tooltip("Casing Ejection Speed")]
+    [SerializeField] private float ejectPower = 150f;
+
     void Start()
     {
         if (barrelLocation == null)
             barrelLocation = transform;
-            
+
         if (gunAnimator == null)
             gunAnimator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        if(isReloading)
-        return;
-        if(currentAmmo <= 0)
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
         {
-            StartCorotine(Reload());
+            StartCoroutine(Reload());
             return;
         }
 
@@ -52,7 +56,7 @@ public class SimpleShoot : MonoBehaviour
     IEnumerator Reload()
     {
         isReloading = true;
-        Debug.Log("Reloading....")
+        Debug.Log("Reloading....");
 
         yield return new WaitForSeconds(reloadTime);
 
@@ -60,35 +64,35 @@ public class SimpleShoot : MonoBehaviour
         isReloading = false;
     }
 
-    void Shoot()
+    public void Shoot()
     {
+        if (currentAmmo <= 0)
+            return;
+
         currentAmmo--;
 
         if (muzzleFlashPrefab)
         {
-            GameObject tempFlash;
-            tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
-
+            GameObject tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
             Destroy(tempFlash, destroyTimer);
         }
 
         if (!bulletPrefab)
-        { return; }
+            return;
 
-        Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+        GameObject bullet = Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
     }
-    
-    void CasingRelease()
+
+    public void CasingRelease()
     {
         if (!casingExitLocation || !casingPrefab)
-        { return; }
+            return;
 
-        GameObject tempCasing;
-        tempCasing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
+        GameObject tempCasing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation);
         tempCasing.GetComponent<Rigidbody>().AddExplosionForce(Random.Range(ejectPower * 0.7f, ejectPower), (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
         tempCasing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(100f, 1000f)), ForceMode.Impulse);
 
         Destroy(tempCasing, destroyTimer);
     }
 }
-
